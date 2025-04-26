706776f63cd93a35ea152dfc32857bfe8395b1ef
Ian Sinnott <3154865+iansinnott@users.noreply.github.com>
2025-04-26 13:41:11 +0800

    1.4.1

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,6 +1,7 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const targetVersion = process.env.npm_package_version;
+const pkg = JSON.parse(readFileSync("package.json", "utf8"));
+const targetVersion = pkg.version;
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
